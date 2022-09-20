f5128e6159605d433b1fa88eba9f820aaf49c5ac
陈玲钰 <1720152558@qq.com>
2022-09-20 14:12:49 +0800

    feat: test

--- config/plugin.js
+++ config/plugin.js
@@ -2,11 +2,6 @@
 
 /** @type Egg.EggPlugin */
 module.exports = {
-  // had enabled by egg
-  // static: {
-  //   enable: true,
-  // }
-
   nunjucks: {
     enable: true,
     package: 'egg-view-nunjucks',
